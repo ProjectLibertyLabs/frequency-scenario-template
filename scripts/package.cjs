0c34fe7fefca7b2d125cfeae6c90f3ddfcddcdeb
Joe Caputo <joseph.caputo@unfinished.com>
2024-06-13 16:47:16 -0400

    fix: make sure types file is hoisted

--- scripts/package.cjs
+++ scripts/package.cjs
@@ -9,6 +9,50 @@ const fs = require('fs');
 // eslint-disable-next-line
 const path = require('path');
 
+const rewriteImportsAndExports = (filePath, outputFilePath) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error reading the file: ${err.message}`);
+      return;
+    }
+
+    const lines = data.split('\n');
+    const rewrittenLines = lines.map((line) => {
+      // Regular expressions to match import and export statements
+      const importExportRegex = /^(import|export)\s.*?from\s*['"](.+?)['"]/;
+      const match = line.match(importExportRegex);
+
+      if (match) {
+        // Extract the existing path
+        const originalPath = match[2];
+
+        // Construct the new path
+        const newPath = originalPath.replace(/^.\//, './mjs/');
+
+        // Replace the old path with the new path in the line
+        line = line.replace(originalPath, newPath);
+      }
+
+      return line;
+    });
+
+    // Join the lines back into a single string
+    const rewrittenData = rewrittenLines.join('\n');
+
+    // Write the modified content to the output file
+    fs.writeFile(outputFilePath, rewrittenData, 'utf8', (err) => {
+      if (err) {
+        console.error(`Error writing the file: ${err.message}`);
+        return;
+      }
+    });
+  });
+};
+
+const typesFilePath = path.join(__dirname, '../dist/mjs/index.d.ts');
+const outputFilePath = path.join(__dirname, '../dist/index.d.ts');
+rewriteImportsAndExports(typesFilePath, outputFilePath);
+
 // Copy over the Readme and License
 fs.copyFileSync(path.join(__dirname, '../README.md'), path.join(__dirname, '../dist/README.md'));
 fs.copyFileSync(path.join(__dirname, '../LICENSE'), path.join(__dirname, '../dist/LICENSE'));
